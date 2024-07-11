b87af59994ad1631941bb1b123e482cbdf0ee592
munucrafts <sarthakmishra520@gmail.com>
2024-07-11 23:28:17 +0530

    Level Design

---
+++ Source/Sarthak_X_KiXAR/Private/Cpp_CarSpawner.cpp
@@ -0,0 +1,111 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cpp_CarSpawner.h"
+
+// Sets default values
+ACpp_CarSpawner::ACpp_CarSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	Spline->SetupAttachment(RootComponent);
+
+	Speed = 100;
+	CarCount = 10;
+	Inverse = false;
+
+	MakeStraigntSpline();
+
+}
+
+void ACpp_CarSpawner::SpawnCarsAlongSpline()
+{
+	float SplineLength = Spline->GetSplineLength();
+	float Interval = SplineLength / CarCount;
+	Distances.SetNum(CarCount);
+
+	for (int i = 0; i < CarCount; i++)
+	{
+		float Distance = Interval * i;
+		Distances[i] = Distance;
+
+		FVector Position = Spline->GetLocationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+		FRotator Rotation = Spline->GetRotationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+
+		AStaticMeshActor* Car = GetWorld()->SpawnActor<AStaticMeshActor>(AStaticMeshActor::StaticClass(), Position, Rotation);
+		int RandInt = FMath::RandRange(0, MeshCollectionArray.Num() - 1);
+		Car->GetComponentByClass<UStaticMeshComponent>()->SetStaticMesh(MeshCollectionArray[RandInt]);
+		Car->SetMobility(EComponentMobility::Movable);
+		CarsArray.Add(Car);
+	}
+}
+
+void ACpp_CarSpawner::MoveAllCars(float DeltaTime)
+{
+	if (!Inverse)
+	{
+		for (int i = 0; i < CarsArray.Num(); i++)
+		{
+			Distances[i] = Distances[i] + DeltaTime * Speed;
+			float SplineLength = Spline->GetSplineLength();
+
+			if (Distances[i] > SplineLength)
+				Distances[i] = 0;
+
+			FVector NewPosition = Spline->GetLocationAtDistanceAlongSpline(Distances[i], ESplineCoordinateSpace::World);
+			FRotator NewRotation = Spline->GetRotationAtDistanceAlongSpline(Distances[i], ESplineCoordinateSpace::World);
+
+			CarsArray[i]->SetActorLocation(NewPosition);
+			CarsArray[i]->SetActorRotation(NewRotation);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < CarsArray.Num(); i++)
+		{
+			Distances[i] = Distances[i] - DeltaTime * Speed;
+			float SplineLength = Spline->GetSplineLength();
+
+			if (Distances[i] < 0)
+				Distances[i] = SplineLength;
+
+			FVector NewPosition = Spline->GetLocationAtDistanceAlongSpline(Distances[i], ESplineCoordinateSpace::World);
+			FRotator NewRotation = Spline->GetRotationAtDistanceAlongSpline(Distances[i], ESplineCoordinateSpace::World);
+			NewRotation.Yaw += 180;
+
+			CarsArray[i]->SetActorLocation(NewPosition);
+			CarsArray[i]->SetActorRotation(NewRotation);
+		}
+	}
+}
+
+void ACpp_CarSpawner::MakeStraigntSpline()
+{
+	for (int i = 0; i < Spline->GetNumberOfSplinePoints(); i++)
+	{
+		Spline->SetSplinePointType(i, ESplinePointType::Linear, true);
+	}
+}
+
+// Called when the game starts or when spawned
+void ACpp_CarSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnCarsAlongSpline();
+}
+
+// Called every frame
+void ACpp_CarSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(CarCount == CarsArray.Num())
+		MoveAllCars(DeltaTime);
+}
+
---
+++ Source/Sarthak_X_KiXAR/Public/Cpp_CarSpawner.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SplineComponent.h"
+#include "Engine/StaticMeshActor.h"
+#include "Cpp_CarSpawner.generated.h"
+
+
+UCLASS()
+class SARTHAK_X_KIXAR_API ACpp_CarSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACpp_CarSpawner();
+
+	UPROPERTY(EditDefaultsOnly)
+	USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USplineComponent* Spline;
+
+	UPROPERTY(EditAnywhere)
+	TArray<UStaticMesh*> MeshCollectionArray;
+
+	UPROPERTY()
+	TArray<AStaticMeshActor*> CarsArray;
+
+	UPROPERTY(EditAnywhere)
+	float Speed;
+
+	UPROPERTY(EditAnywhere)
+	float CarCount;
+
+	UPROPERTY(EditAnywhere)
+	bool Inverse;
+
+	UPROPERTY()
+	TArray<float> Distances;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void MakeStraigntSpline();
+
+	UFUNCTION()
+	void SpawnCarsAlongSpline();
+
+	UFUNCTION()
+	void MoveAllCars(float DeltaTime);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
